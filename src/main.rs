b227b1bdcae344eac15eb95b11cd37cd43a41913
Jacob Lauxman <jlauxman@gmail.com>
2024-03-09 13:23:55 -0700

    3rd stage attempt

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,51 @@
-use std::io::{Read, Result, Write};
+use anyhow::{Context, Result};
+use std::io::{BufRead, BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 
-fn handler(mut stream: TcpStream) -> Result<()> {
-    // reading
-    let mut buf = vec![];
-    let _ = stream.read(&mut buf).unwrap();
+const RESPONSE_OK: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const RESPONSE_NOTFOUND: &str = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+
+fn handler(mut stream: TcpStream) -> Result<(), anyhow::Error> {
+    // -- reading --
+    let buf = BufReader::new(&mut stream);
+    let req: Vec<_> = buf
+        .lines()
+        .map(|res| res.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let start_line = req[0].split_whitespace().collect::<Vec<_>>();
+    let (_method, path, _ver) = (start_line[0], start_line[1], start_line[2]);
 
     // writing
-    stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+    match path {
+        "/" => stream
+            .write_all(RESPONSE_OK.as_bytes())
+            .context("Failed to write OK response")?,
+        _ => stream
+            .write_all(RESPONSE_NOTFOUND.as_bytes())
+            .context("Failed to write NOT FOUND response")?,
+    }
+
+    Ok(())
 }
 
-fn main() {
-    let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
+fn main() -> Result<()> {
+    let listener =
+        TcpListener::bind("127.0.0.1:4221").context("Failed to bind TCP Listener to port 4221")?;
     println!("Listening on port {}", listener.local_addr().unwrap());
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handler(stream).unwrap();
+                handler(stream)?;
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+
+    Ok(())
 }
